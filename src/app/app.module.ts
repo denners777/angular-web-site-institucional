899d53a3a2bff6dc27d3bf3123bf6ca1377e8251
Denner Fernandes <denners777@hotmail.com>
2018-06-09 18:20:12 -0300

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { QuemSomosComponent } from './quem-somos/quem-somos.component';
-import { ContatoComponent } from './contato/contato.component';
 import { NossaVisaoComponent } from './nossa-visao/nossa-visao.component';
+import { ContatoModule } from './contato/contato.module';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule,
+    HttpModule,
+    RouterModule.forRoot([]),
+    ContatoModule
   ],
   declarations: [
     AppComponent,
     QuemSomosComponent,
-    ContatoComponent,
     NossaVisaoComponent
   ],
   providers: [],
---
+++ src/app/contato/contato.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {ContatoComponent} from './contato.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: 'contato', component: ContatoComponent }
+    ])
+  ],
+  declarations: [
+    ContatoComponent
+  ],
+  exports: [
+    ContatoComponent
+  ]
+})
+export class ContatoModule { }
